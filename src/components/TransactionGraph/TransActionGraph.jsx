8bdf3f38afe1eaaa3c2bcf2d37a55ff0d710cb00
Omar-Abosea3 <oa615010@gmail.com>
2024-07-14 00:15:39 +0300

    first commit

--- src/components/TransactionGraph/TransActionGraph.jsx
+++ src/components/TransactionGraph/TransActionGraph.jsx
@@ -5,24 +5,69 @@ import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const TransactionGraph = ({ transactions }) => {
-  const dates = transactions?.map((transaction) => transaction.date);
-  const amounts = transactions?.map((transaction) => transaction.amount);
-
-  const data = {
-    labels: dates,
-    datasets: [
-      {
-        label: 'Transaction Amount',
-        data: amounts,
-        borderColor: 'rgba(75,192,192,1)',
-        borderWidth: 2,
-        fill: false,
+    if (!Object.groupBy) {
+      Object.groupBy = function (iterable, callbackFn) {
+        const result = {};
+        for (const [index, element] of iterable.entries()) {
+          const key = callbackFn(element, index);
+          if (!result[key]) {
+            result[key] = [];
+          }
+          result[key].push(element);
+        }
+        return result;
+      };
+    }
+    const displayedDates = Object.groupBy(transactions, ({ date }) => date);
+  
+    for (const key in displayedDates) {
+      if (Object.hasOwnProperty.call(displayedDates, key)) {
+        displayedDates[key] = displayedDates[key].reduce(
+          (sum, item) => sum + item.amount,
+          0
+        );
+      }
+    }
+  
+    console.log({ displayedDates });
+    const dates = Object.keys(displayedDates);
+    const amounts = Object.values(displayedDates);
+  
+    const data = {
+      labels: dates,
+      datasets: [
+        {
+          label: "Transaction",
+          data: amounts,
+          borderColor: "tomato",
+          borderWidth: 4,
+          fill: false,
+        },
+      ],
+    };
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+          title: {
+            display: true,
+            text: "Transaction Amount",
+          },
+        },
+        x: {
+          title: {
+            display: true,
+            text: "Date",
+          },
+        },
       },
-    ],
+    };
+    return (
+      <div className="mt-3">
+        <Line data={data} options={options} />
+      </div>
+    );
   };
-
-  return <Line data={data} />;
-};
 
 export default TransactionGraph;
 
